3ef7d9f27230e089946beed341b77e321505a821
roni <ronimia5141@gmail.com>
2024-12-19 00:18:32 +0600

    html project add

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+jQuery('.partners-slider').owlCarousel({
+	loop: true,
+	responsiveClass: true,
+	autoplay: true,
+	nav: false,
+	
+	margin:20,
+	dots: true,
+	autoplayTimeout: 5000,
+	responsive: {
+	  0: {
+		items: 1,
+		dotsEach: 3,
+	  },
+	  500: {
+		items: 2,
+	  },
+	  991: {
+		items: 4,
+	  },
+	  1366: {
+		items: 6,
+	  },
+	  1700: {
+		items: 7,
+	    stagePadding: 50,     
+	  }
+	}
+  });
+  
+
+  jQuery(document).ready(function(){
+	jQuery("#scrollTopButton").click(function(){
+	  jQuery("html, body").animate({scrollTop: 0}, 800);
+	})
+  });
